bedf5af815cd1b1b188eeb254b0e1eb7c0ddd5b4
naiaraodiaga <naiaraodiaga@gmail.com>
2014-05-06 15:13:07 +0200

    Ejercicio Manifiesto

---
+++ PruebaManifiesto/src/com/naiaraodiaga/pruebamanifiesto/MyReceiver.java
@@ -0,0 +1,14 @@
+package com.naiaraodiaga.pruebamanifiesto;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	    // TODO Auto-generated method stub
+	}
+
+}
