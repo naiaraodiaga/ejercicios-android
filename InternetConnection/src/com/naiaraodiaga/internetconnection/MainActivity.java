f2f32cb5b81050b3419701b3abb5c0887dc4e33f
iMac 14 <naiaraodiaga@gmail.com>
2014-05-15 13:29:07 +0200

    DownloadManager

--- InternetConnection/src/com/naiaraodiaga/internetconnection/MainActivity.java
+++ InternetConnection/src/com/naiaraodiaga/internetconnection/MainActivity.java
@@ -1,34 +1,40 @@
 package com.naiaraodiaga.internetconnection;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.DownloadManager;
+import android.app.DownloadManager.Query;
+import android.app.DownloadManager.Request;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.os.Build;
 
 public class MainActivity extends Activity {
+
+	private long reference;
+	private BroadcastReceiver receiver;
+	private DownloadManager downloadManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +50,65 @@ public class MainActivity extends Activity {
 		};
 		Button downloadButton = (Button) findViewById(R.id.downloadButton);
 		downloadButton.setOnClickListener(onClick);
+
+		IntentFilter filter = new IntentFilter(
+				DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+
+		receiver = new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+
+				Log.d("NAIARA", "Downloading");
+				
+				long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+				if(id == reference) {
+					Query q = new Query();
+					q.setFilterById(id);
+					
+					Cursor downloads = downloadManager.query(q);
+					
+					int nameIdx = downloads.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME);
+					int sizeIdx = downloads.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES);
+					while(downloads.moveToNext()) {
+						// Obtener los datos
+						String name = downloads.getString(nameIdx);
+						int size = downloads.getInt(sizeIdx);
+						
+						Log.d("NAIARA", "Nombre : " + name);
+						Log.d("NAIARA", "Tama–o : " + String.valueOf(size));
+						
+						Intent testIntent = new Intent();
+			            testIntent.setAction(Intent.ACTION_VIEW);
+			            File f = new File(name);
+			            Uri uri = Uri.fromFile(f);
+
+			            startActivity(testIntent);
+						
+					}
+				}
+			}
+		};
+
+		registerReceiver(receiver, filter);
+
+		OnClickListener onClickZip = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				download();
+			}
+		};
+		Button downloadZipButton = (Button) findViewById(R.id.downloadZipButton);
+		downloadZipButton.setOnClickListener(onClickZip);
+
 		
+
 	}
 
-	public void initThread(){
-		Thread t = new Thread(new Runnable(){
+	public void initThread() {
+		Thread t = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
@@ -60,49 +120,73 @@ public class MainActivity extends Activity {
 		});
 		t.start();
 	}
-	
-	public void downloadPhotos() throws JSONException{
-		try{
+
+	public void downloadPhotos() throws JSONException {
+		try {
 			String ruta = getString(R.string.ruta);
-			URL url = new URL (ruta);
-			
-			HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
-			
+			URL url = new URL(ruta);
+
+			HttpURLConnection httpConnection = (HttpURLConnection) url
+					.openConnection();
+
 			int responseCode = httpConnection.getResponseCode();
 			if (responseCode == HttpURLConnection.HTTP_OK) {
-			  InputStream in = httpConnection.getInputStream();
-			  processStream(in);
+				InputStream in = httpConnection.getInputStream();
+				processStream(in);
 			}
-		}
-		catch(MalformedURLException e1){
-			Log.d("NAIARA", "Malformed URL Exception: "+ e1);
-		}
-		catch(IOException e2){
+		} catch (MalformedURLException e1) {
+			Log.d("NAIARA", "Malformed URL Exception: " + e1);
+		} catch (IOException e2) {
 			Log.d("NAIARA", "IO Exception: " + e2);
 		}
 	}
-	
-	public void processStream(InputStream in) throws IOException, JSONException{
+
+	public void processStream(InputStream in) throws IOException, JSONException {
 		JSONObject json = new JSONObject();
 		JSONArray array = new JSONArray();
-		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(in,
+				"UTF-8"));
 		StringBuilder sb = new StringBuilder();
 		String line = null;
-		
-        while ((line = bf.readLine()) != null) {
-            sb.append(line);
-        }
-        json = new JSONObject(sb.toString());
-        
-        array = json.getJSONArray("photos");
-        for (int i = 0; i < array.length(); i++) {
-			JSONObject photo = array.getJSONObject(i);
-			Log.d("NAIARA", "Name: "+photo.getString("name")+"\t URL: "+photo.getString("image_url"));
+
+		while ((line = bf.readLine()) != null) {
+			sb.append(line);
 		}
-        
-		
-		
+		json = new JSONObject(sb.toString());
+
+		array = json.getJSONArray("photos");
+		for (int i = 0; i < array.length(); i++) {
+			JSONObject photo = array.getJSONObject(i);
+			Log.d("NAIARA", "Name: " + photo.getString("name") + "\t URL: "
+					+ photo.getString("image_url"));
+		}
+	}
+
+	public void download() {
+		try {
+
+			String serviceString = Context.DOWNLOAD_SERVICE;
+			downloadManager = (DownloadManager) getSystemService(serviceString);
+
+			Uri uri = Uri
+					.parse("http://developer.android.com/shareables/icon_templates-v4.0.zip");
+			DownloadManager.Request request = new Request(uri);
+			// request.setAllowedNetworkTypes(Request.NETWORK_WIFI);
+			request.setNotificationVisibility(Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+			
+			// Por defecto las descargas se guardan aqu’
+			// /data/data/com.android.providers.downloads/cache "id" "name"
+			// Es mejor hacerlo aqu’
+			// /mnt/sdcard/Android/data/com.___.downloads/files/Download
+			
+			request.setDestinationInExternalFilesDir(this,
+					Environment.DIRECTORY_DOWNLOADS, uri.getLastPathSegment());
+
+			reference = downloadManager.enqueue(request);
+		} catch (Exception e) {
+			Log.d("NAIARA", "Exception: " + e.getMessage());
+		}
 	}
 
 }
